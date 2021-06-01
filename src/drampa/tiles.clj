40eb028efc26582044bbe50b26b2b40c60b0e51c
Jayce Mitchell <chimerror@live.com>
2021-05-31 20:31:02 -0700

    Adding tiles to make up a wall

--- src/drampa/tiles.clj
+++ src/drampa/tiles.clj
@@ -3,3 +3,13 @@
 (defrecord Tile [suit rank])
 
 (def tile-suits [:pin :sou :man :zi])
+(def number-tiles
+  (for [current-tile (range 4)
+        suit [:pin :sou :man]
+        rank (range 1 10)]
+    (->Tile suit (cond (and (= current-tile 0) (= rank 5)) 0 :else rank))))
+(def honor-tiles
+  (for [current-tile (range 4)
+        rank (range 1 8)]
+    (->Tile :zi rank)))
+(def sorted-wall (vec (concat number-tiles honor-tiles)))
--- test/drampa/tiles_test.clj
+++ test/drampa/tiles_test.clj
@@ -2,6 +2,22 @@
   (:require [clojure.test :refer :all]
             [drampa.tiles :refer :all]))
 
-(deftest there-are-four-suits
-  (testing "Are there 4 suits"
+(deftest there-are-the-correct-number-of-suits
+  (testing "Are there 4 suits?"
     (is (= (count tile-suits) 4))))
+
+(deftest there-are-the-correct-number-of-number-tiles
+  (testing "Are there 108 number tiles?"
+    (is (= (count number-tiles) 108))))
+
+(deftest there-are-the-correct-number-of-red-fives
+  (testing "Are there 3 red fives (Rank 0)?"
+    (is (= (count (filter #(= (:rank %) 0) number-tiles)) 3))))
+
+(deftest there-are-the-correct-number-of-honor-tiles
+  (testing "Are there 28 honor tiles?"
+    (is (= (count (filter #(= (:suit %) :zi) honor-tiles)) 28))))
+
+(deftest there-are-the-correct-number-of-tiles-in-a-wall
+  (testing "Are there 136 tiles in a wall?"
+    (is (= (count sorted-wall) 136))))
