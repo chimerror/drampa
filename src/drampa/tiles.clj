2f7309c9ed79c85f9d3b9f04de06b9af4255a97c
Jayce Mitchell <chimerror@live.com>
2021-06-01 17:35:31 -0700

    Adding compare-tiles for sorting, adding to-from MPSZ notation

--- src/drampa/tiles.clj
+++ src/drampa/tiles.clj
@@ -2,6 +2,9 @@
 
 (defrecord Tile [suit rank])
 
+(defn Tile->str [tile]
+  (str (:rank tile) (second (str (:suit tile)))))
+
 (def tile-suits [:pin :sou :man :zi])
 (def number-tiles
   (for [current-tile (range 4)
@@ -12,4 +15,42 @@
   (for [current-tile (range 4)
         rank (range 1 8)]
     (->Tile :zi rank)))
-(def sorted-wall (vec (concat number-tiles honor-tiles)))
+(def initial-wall (vec (concat number-tiles honor-tiles)))
+
+(defn compare-tiles [x y]
+  (let [{x-suit :suit x-rank :rank} x
+        {y-suit :suit y-rank :rank} y]
+    (cond (not= x-suit y-suit) (apply compare (map #(.indexOf tile-suits %) [x-suit y-suit]))
+          (= x-rank y-rank) 0
+          (= x-rank 0) (if (<= y-rank 5) 1 -1)
+          (= y-rank 0) (if (<= x-rank 5) -1 1)
+          :else (compare x-rank y-rank))))
+
+(def notation-group-regex #"([0-9]+)([psm])|([1-7]+)(z)")
+
+(defn- tiles-from-notation-group [notation-group]
+  (let [filtered-notation-group (vec (remove nil? notation-group))
+        tile-numbers (seq (get filtered-notation-group 1))
+        suit-letter (get filtered-notation-group 2)
+        suit (cond (= suit-letter "p") :pin (= suit-letter "s") :sou (= suit-letter "m") :man :else :zi)]
+    (map #(->Tile suit (Integer/parseInt (str %))) tile-numbers)))
+
+(defn tiles-from-notation [notation]
+  (apply concat (map tiles-from-notation-group (re-seq notation-group-regex notation))))
+
+(defn notation-from-tiles [tiles]
+  (loop  [current-tile (first tiles)
+          tiles-left (next tiles)
+          result []]
+    (if (nil? current-tile)
+        (apply str result)
+        (let [current-suit (:suit current-tile)
+              next-tile (first tiles-left)
+              next-suit (:suit next-tile)
+              full-tile-string (Tile->str current-tile)
+              title-string
+                (if (or (nil? next-tile) (not= next-suit current-suit))
+                  full-tile-string
+                  (first full-tile-string))]
+          (recur (first tiles-left) (next tiles-left) (conj result title-string))))))
+
--- test/drampa/tiles_test.clj
+++ test/drampa/tiles_test.clj
@@ -20,4 +20,26 @@
 
 (deftest there-are-the-correct-number-of-tiles-in-a-wall
   (testing "Are there 136 tiles in a wall?"
-    (is (= (count sorted-wall) 136))))
+    (is (= (count initial-wall) 136))))
+
+(deftest there-are-the-correct-tiles-in-a-wall
+  (testing "Are the expected tiles in a wall?"
+    (is (=
+      (notation-from-tiles (sort compare-tiles initial-wall))
+      (str
+        "111122223333444455506666777788889999p111122223333444455506666777788889999s" "111122223333444455506666777788889999m1111222233334444555566667777z")))))
+
+(deftest can-round-trip-tiles-from-notation
+  (testing "Can we perform a round-trip through tiles-from-notation?"
+    (let [original-notation "19p19s19m1234567z1m"
+          tiles (tiles-from-notation original-notation)
+          round-trip-notation (notation-from-tiles tiles)]
+      (is (= original-notation round-trip-notation)))))
+
+(deftest can-sort-tiles-using-compare-tiles
+  (testing "Can tiles be sorted using compare-tiles?"
+    (let [original-notation "12357z19p19s19m1m05p0m64z"
+          tiles (sort compare-tiles (tiles-from-notation original-notation))
+          round-trip-notation (notation-from-tiles tiles)]
+      (is (= round-trip-notation "1509p19s1109m1234567z")))))
+
