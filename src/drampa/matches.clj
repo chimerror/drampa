28990eb92fb6e78cd85c53ebd5b344e228d0043d
Jayce Mitchell <chimerror@live.com>
2021-06-01 23:01:30 -0700

    Filling in player winds

--- src/drampa/matches.clj
+++ src/drampa/matches.clj
@@ -1,11 +1,22 @@
 (ns drampa.matches
   (:require [drampa.tiles :as d.tiles]))
 
-(defrecord Match [wall scores])
+(defrecord Match [wall scores player-winds])
 
 (def starting-score 30000)
 
+(defn fill-starting-winds [starting-index]
+  (loop  [current-index starting-index
+          winds [:east :south :west :north]
+          result (vec (repeat 4 nil))]
+    (if (empty? winds)
+      result
+      (let [next-index (if (= current-index 3) 0 (inc current-index))
+            wind (first winds)]
+        (recur next-index (next winds) (assoc result current-index wind))))))
+
 (defn get-initial-match []
   (let [wall (shuffle d.tiles/initial-wall)
-        scores (vec (repeat 4 starting-score))]
-  (->Match wall scores)))
+        scores (vec (repeat 4 starting-score))
+        player-winds (fill-starting-winds (rand-int 4))]
+  (->Match wall scores player-winds)))
--- test/drampa/matches_test.clj
+++ test/drampa/matches_test.clj
@@ -3,11 +3,18 @@
             [drampa.matches :refer :all]))
 
 (deftest get-initial-match-is-correct
-  (let [{:keys [wall scores]} (get-initial-match)]
+  (let [{:keys [wall scores player-winds]} (get-initial-match)]
     (testing "Is the starting wall of a match initialied correctly?"
       (is (not (nil? wall)))
       (is (= (count wall)) 136))
     (testing "Are the starting scores of a match initialied correctly?"
       (is (not (nil? scores)))
       (is (= (count scores) 4))
-      (is (every? #(= starting-score %) scores)))))
+      (is (every? #(= starting-score %) scores)))
+    (testing "Are the seat winds of a match initialied correctly?"
+      (is (= (count player-winds) 4))
+      (is (distinct? player-winds))
+      (is (some #{:east} player-winds))
+      (is (some #{:south} player-winds))
+      (is (some #{:west} player-winds))
+      (is (some #{:north} player-winds)))))
